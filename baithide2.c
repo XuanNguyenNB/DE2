e1a03b00cec6b718a47cef7573a1f50a6430f5eb
XuanNguyenNB <107878677+XuanNguyenNB@users.noreply.github.com>
2022-11-09 15:15:00 +0700

    Update baithide2.c

--- baithide2.c
+++ baithide2.c
@@ -25,3 +25,30 @@ int main()
     }
     return 0;
 }
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main()
+{
+    char pid[100];
+    int rc = fork();
+    if (rc < 0) {
+        printf("fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0){
+      execlp("/bin/top", "/bin/top", NULL);
+    }
+     else {
+        // parent goes down this path (original process)
+        int wc = wait(NULL);
+        printf("hello, I am parent\n");
+        printf("enter the pid to kill: \n");
+        scanf("%s",pid);
+        execlp("kill", "kill", "-9", pid, NULL);
+    }
+    return 0;
+}
